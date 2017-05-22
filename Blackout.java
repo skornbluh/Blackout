ac3361188a26171c1bc6cee5f4ba074050562dd8
skornbluh <skornbluh@gmail.com>
2017-05-22 15:24:41 -0500

    Add files via upload

--- Blackout.java
+++ Blackout.java
@@ -2,52 +2,117 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Blackout {
-	public static double answer = 0.0;
+
+	private static String equation;
 	public static String parsedEquation = "";
-	
-	public static String getData(String fileName) {
-		String data = "";
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader(fileName));
-			StringBuilder sb = new StringBuilder();
-			String line = "";
-			do {
-				line = br.readLine();
-				if (line != null) {
-					sb.append(line);
-				}
-			} while (line != null);
-			data = sb.toString();
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(0);
-		} 
-		return data;
-	}
-	
-	/**
-	 * makes sure equation is valid, evaluates equation and stores the answer as a class variable
-	 * @param equation
-	 * @return the valid expression as a string, or empty string if expression is invalid
-	 */
-	private static boolean parseExpression(String equation) {
-		char[] charArray = equation.toCharArray();
-		boolean isValid = isValidExpression(equation);
-		double answer1 = 0.0, answer2 = 0.0;
-		String[] expr = equation.split("=");
+	public static String blackedOutEquation;
+	private static char firstBlackoutCharacter;
+	private static char secondBlackoutCharacter;
+	private static double total = 0;
+
+	// this black outs numbers
+	public static String blackoutNumbers(String equation) {
+		blackedOutEquation = null;
+
 		for (int i = 0; i < equation.length(); i++) {
-			answer1 = evaluate(expr[0]);
-			answer2 = evaluate(expr[1]);
-		}	
-		return answer1 == answer2;
+			firstBlackoutCharacter = equation.charAt(i);
+			for (int j = i + 1; j < equation.length(); j++) {
+				secondBlackoutCharacter = equation.charAt(j);
+
+				blackedOutEquation = equation.substring(0, i) + equation.substring(i + 1, j)
+						+ equation.substring(j + 1, equation.length());
+
+				boolean result = isValidExpression(blackedOutEquation);
+
+				if (result == true) {
+					boolean result2 = parseExpression(blackedOutEquation);
+					if (result2 == true) {
+						System.out.println("Blacked out: " + firstBlackoutCharacter + ", " + secondBlackoutCharacter);
+						System.out.println("Resulting equations: " + blackedOutEquation);
+						System.out.println("Value: " + total);
+
+					}
+				}
+
+			}
+
+		}
+
+		return blackedOutEquation;
+
 	}
 
-	
-	private static int evaluate(String string) {
-		
-		return 0;
+	public static void main(String[] args) {
+		String equation = args[0];
+
+		blackoutNumbers(equation);
+
+	}
+
+	private static double evaluate(String string) {
+
+		// Get all of the numbers out of whichever side of the equation is being
+		// evaluated
+		String[] expressionNumbers = string.split("[*+-/%]");
+
+		// This variable holds the running total as we loop through the equation
+		// and do the calculations
+		total = 0;
+		int operatorsFoundCounter = 0;
+
+		for (int i = 0; i < string.length(); i++) {
+			char character = string.charAt(i);
+			// If the character is an operator, then we want to do a calculation
+			if (isOperator(character)) {
+
+				int firstNumber = Integer.parseInt(expressionNumbers[operatorsFoundCounter]);
+				int secondNumber = Integer.parseInt(expressionNumbers[operatorsFoundCounter + 1]);
+
+				// Increment the counter every time we find an operator
+				operatorsFoundCounter++;
+
+				int result = 0;
+				switch (character) {
+				case '+':
+					result += firstNumber + secondNumber;
+					break;
+				case '-':
+					result += firstNumber - secondNumber;
+					break;
+				case '/':
+					result += firstNumber / secondNumber;
+					break;
+				case '*':
+					result += firstNumber * secondNumber;
+					break;
+				case '%':
+					result += firstNumber % secondNumber;
+					break;
+				default:
+					throw new RuntimeException("unkonwn operator: " + character);
+				}
+
+				expressionNumbers[operatorsFoundCounter] = Integer.toString(result);
+				// add the result of this operation to the running total
+				total = result;
+			}
+		}
+
+		// If you never find any operators, then the string must be the result
+
+		if (operatorsFoundCounter == 0) {
+			total = Integer.parseInt(string);
+		}
+
+		return total;
+
+	}
+
+	private static boolean isOperator(char symbol) {
+		if (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/' || symbol == '%' || symbol == '=') {
+			return true;
+		}
+		return false;
 	}
 
 	private static boolean isValidExpression(String equation) {
@@ -62,31 +127,30 @@ public class Blackout {
 			return false;
 		}
 		// ensure that we do not have two or more consecutive operators
-		// valid operators are: +-*/%
 		for (int i = 0; i < equation.length() - 1; i++) {
-			if (isOperator(equation.charAt(i)) && isOperator(equation.charAt(i+1))) {
+			if (isOperator(equation.charAt(i)) && isOperator(equation.charAt(i + 1))) {
 				return false;
 			}
 		}
-		
+
+		// ensures that equation does not start or end with an operator
+		if (isOperator(equation.charAt(0)) || isOperator(equation.charAt(equation.length() - 1))) {
+			return false;
+		}
+
 		return true;
 	}
 
-	private static boolean isOperator(char symbol) {
-		if (symbol == '+' || symbol == '-'|| symbol == '*'|| symbol == '/'|| symbol == '%') {
-			return true;
-		}
-		
-		return false;
-	}
+	private static boolean parseExpression(String equation) {
+		String[] expression = equation.split("=");
 
-	public static void main(String[] args) {
-		String equation = getData("test_blackout_01.txt");
-		System.out.println("Equation is: " + equation);
-		if (parseExpression(equation)) {
-			System.out.println("Equation " + parsedEquation + " is invalid.");
+		double answer1 = evaluate(expression[0]);
+		double answer2 = evaluate(expression[1]);
+
+		if (answer1 == answer2) {
+			return true;
 		} else {
-			System.out.println("Equation " + parsedEquation + " is valid.");
+			return false;
 		}
 
 	}
